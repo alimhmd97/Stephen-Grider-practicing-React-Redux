7adc471081d459c827d95fcd6535e300ebd7c428
alimhmd97 <aliiimhmddd99@gmail.com>
2022-02-08 02:11:15 +0200

    adding reducers-and-action

--- src/index.js
+++ src/index.js
@@ -7,7 +7,7 @@ import { Provider } from "react-redux";
 import { createStore } from "redux";
 import Reducers from "./reducers";
 const App = (
-  <Provider>
+  <Provider store={createStore(Reducers)}>
     <App />
   </Provider>
 );
